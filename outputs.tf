7f8cb6226f2a014722d5f24fb142c2f74a049e96
esaenz@mcclatchyservices.com <esaenz@mcclatchyservices.com>
2022-06-08 22:01:40 -0500

    Fixing outputs template

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,4 @@
-output "service_account_nonprod_id" {
-  value = google_service_account.service_account_nonprod.id
+output "output1" {
+  value = "value1"
 }
 
-output "service_account_prod_id" {
-  value = google_service_account.service_account_prod.id
-}
-
-output "service_account_build_id" {
-  value = google_service_account.service_account_build.id
-}
