2ecc18b533e58d54b99204fd11dcf3a066e6844d
esaenz@mcclatchyservices.com <esaenz@mcclatchyservices.com>
2022-06-21 07:02:40 -0500

    fixing resources's names

--- modules/transit-vpc/main.tf
+++ modules/transit-vpc/main.tf
@@ -1,15 +1,14 @@
-resource "google_compute_network" "vpc_transit_network" {
+resource "google_compute_network" "vpc_network" {
   name                    = "vpc-it-wsh-${var.name_prefix}-001"
   project                 = var.gcp_project_id
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "vpc_transit_network" {
+resource "google_compute_subnetwork" "vpc_network" {
   name                     = "sbnt-it-wsh-${var.name_prefix}-eus1-001"
   ip_cidr_range            = var.ip_range
   region                   = var.gcp_region
   network                  = google_compute_network.vpc_transit_network.self_link
   private_ip_google_access = true
-
 }
 
