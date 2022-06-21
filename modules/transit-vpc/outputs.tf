f6a852da56cd35f07a9b5b377b974d345ab2af83
esaenz@mcclatchyservices.com <esaenz@mcclatchyservices.com>
2022-06-21 06:49:03 -0500

    Updating peering module and adding changes to the outputs

--- modules/transit-vpc/outputs.tf
+++ modules/transit-vpc/outputs.tf
@@ -1,19 +1,19 @@
-output gcp_region {
+output "gcp_region" {
   value = var.gcp_region
 }
 
-output network_id {
+output "network_id" {
   value = google_compute_network.vpc_network.id
 }
 
-output network_name {
+output "network_name" {
   value = google_compute_network.vpc_network.name
 }
 
-output network_link {
+output "network_link" {
   value = google_compute_network.vpc_network.self_link
 }
 
-output network {
+output "network" {
   value = google_compute_network.vpc_network
 }
