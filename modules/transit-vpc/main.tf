712c355abeee08365969d0d6e23f464d0429a35e
esaenz@mcclatchyservices.com <esaenz@mcclatchyservices.com>
2022-06-21 07:05:17 -0500

    Fixing references between resources

--- modules/transit-vpc/main.tf
+++ modules/transit-vpc/main.tf
@@ -8,7 +8,7 @@ resource "google_compute_subnetwork" "vpc_network" {
   name                     = "sbnt-it-wsh-${var.name_prefix}-eus1-001"
   ip_cidr_range            = var.ip_range
   region                   = var.gcp_region
-  network                  = google_compute_network.vpc_transit_network.self_link
+  network                  = google_compute_network.vpc_network.self_link
   private_ip_google_access = true
 }
 
