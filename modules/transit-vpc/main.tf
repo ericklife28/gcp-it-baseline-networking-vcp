cff437c71c0eca753ef89efccba38f0e747dcc87
esaenz@mcclatchyservices.com <esaenz@mcclatchyservices.com>
2022-06-21 06:49:03 -0500

    Updating peering module and adding changes to the outputs

--- modules/transit-vpc/main.tf
+++ modules/transit-vpc/main.tf
@@ -1,10 +1,10 @@
-resource google_compute_network vpc_transit_network {
+resource "google_compute_network" "vpc_transit_network" {
   name                    = "vpc-it-wsh-${var.name_prefix}-001"
   project                 = var.gcp_project_id
   auto_create_subnetworks = false
 }
 
-resource google_compute_subnetwork vpc_transit_network {
+resource "google_compute_subnetwork" "vpc_transit_network" {
   name                     = "sbnt-it-wsh-${var.name_prefix}-eus1-001"
   ip_cidr_range            = var.ip_range
   region                   = var.gcp_region
--- modules/transit-vpc/variables.tf
+++ modules/transit-vpc/variables.tf
@@ -1,20 +1,20 @@
-variable gcp_region {
+variable "gcp_region" {
   description = "The region for this network (default us-east1)"
   type        = string
   default     = "us-east1"
 }
 
-variable gcp_project_id {
+variable "gcp_project_id" {
   description = "The project for this network"
   type        = string
 }
 
-variable name_prefix {
+variable "name_prefix" {
   description = "Network object name prefix"
   type        = string
 }
 
-variable ip_range {
+variable "ip_range" {
   description = "Primary ip subnet cidr"
   type        = string
 }
